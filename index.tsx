b9d7004b1ab211f0706c7892aae554aad870954f
Rishabh Singh <ris8645208@gmail.com>
2023-10-09 14:01:16 +0530

    feat: add routing in simple http server

--- index.tsx
+++ index.tsx
@@ -3,7 +3,19 @@ import Bun from 'bun';
 const server = Bun.serve({
   port: 3000,
   fetch(request) {
-    return new Response("Welcome to Bun!");
+    console.log(request);
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    if (path === '/') {
+      return new Response("Welcome to Bun!");
+    } else if (path === '/about') {
+      return new Response("This is the about page.");
+    } else if (path === '/contact') {
+      return new Response("This is the contact page.");
+    } else {
+      return new Response("Page not found.", { status: 404 });
+    }
   },
 });
 
