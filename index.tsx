b6e0ff4045c433e5e86fc16255c78269a3a92b6a
Rishabh Singh <ris8645208@gmail.com>
2023-10-09 13:42:28 +0530

    feat: add basic http server

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import Bun from 'bun';
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(request) {
+    return new Response("Welcome to Bun!");
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
